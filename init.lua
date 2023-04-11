dc4d68325f344d8294a4ad3f1f929364737e4318
Kalyan Mudumby <71078441+theinhumaneme@users.noreply.github.com>
2023-04-12 02:06:40 +0530

    terminal plugin added

--- init.lua
+++ init.lua
@@ -171,7 +171,8 @@ require('lazy').setup({
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
   },
-
+  {'akinsho/toggleterm.nvim', version = "*", config = true},
+  
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
